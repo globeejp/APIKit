fca7ead61434f6bc4fe9f4d43cc299e99d74b0ca
Yoshihiro Kato <kato_yoshihiro@cyberagent.co.jp>
2024-07-26 17:23:43 +0900

    fixes minor issue

--- Sources/APIKit/Error/ErrorObject.swift
+++ Sources/APIKit/Error/ErrorObject.swift
@@ -1,7 +1,7 @@
 public final class ErrorObject<Value>: @unchecked Sendable {
     public let value: Value
 
-    init(value: Value) {
+    public init(value: Value) {
         self.value = value
     }
 }
